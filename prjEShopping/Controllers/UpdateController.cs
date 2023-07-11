ab286868ab322c0ffd7cbb372550fa4b35d57f04
allen <abamamatw@gmail.com>
2023-07-11 15:56:40 +0800

    update

---
+++ prjEShopping/Controllers/UpdateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class UpdateController : Controller
+    {
+        // GET: Update
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
