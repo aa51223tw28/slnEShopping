78e15ab65f606d9c1c655e16fb058169d6bd25c9
MuMu <tim80031@hotmail.com>
2023-07-11 20:58:56 +0800

    6

---
+++ prjEShopping/Controllers/SellerShipmentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class SellerShipmentController : Controller
+    {
+        // GET: SellerShipment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult ShipmentList()
+		{
+			return View();
+		}
+	}
+}
