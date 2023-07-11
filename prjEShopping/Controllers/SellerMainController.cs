a9a3f2969e081474d59aac4053d24fc0e0e0de34
MuMu <tim80031@hotmail.com>
2023-07-11 20:37:18 +0800

    index+layout

---
+++ prjEShopping/Controllers/SellerMainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class SellerMainController : Controller
+    {
+        // GET: SellerMain
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
