68747356e78edcaaf5d728dbe219188556f33109
SieJH <sjh.jason@gmail.com>
2023-07-11 18:13:26 +0800

    Revert "0711_View變數測試_連接資料庫"
    This reverts commit b3b4d2fd3c5ba525027db05b9c9b3a41bb27ee28.

--- prjEShopping/Controllers/AdminController.cs
+++ prjEShopping/Controllers/AdminController.cs
@@ -1,7 +1,5 @@
-﻿using prjEShopping.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,10 +11,7 @@ namespace prjEShopping.Controllers
         // GET: Admin
         public ActionResult List()
         {
-           EShoppingEntities db= new EShoppingEntities();
-            List<Admin> admins = db.Admins.ToList();
-            return View(admins);
+            return View();
         }
-
     }
 }
