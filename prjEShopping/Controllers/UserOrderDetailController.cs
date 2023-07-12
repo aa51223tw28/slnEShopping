4041eb3315a42c77d3c269f0f0dd680980c96be0
SIH CI ZENG <aa51223tw28@gmail.com>
2023-07-12 16:12:22 +0800

    UserOrderDetailController

---
+++ prjEShopping/Controllers/UserOrderDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class UserOrderDetailController : Controller
+    {
+        // GET: UserOrderDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
