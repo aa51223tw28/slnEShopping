c7c11aa05d54a0fb0fae93fdb9302b067fefeb80
SIH CI ZENG <aa51223tw28@gmail.com>
2023-07-12 16:39:18 +0800

    UserPurchaseListController

---
+++ prjEShopping/Controllers/UserPurchaseListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class UserPurchaseListController : Controller
+    {
+        // GET: UserPurchaseList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
