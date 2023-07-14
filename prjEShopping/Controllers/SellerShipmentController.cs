00118af183a3afb77f9e60e458fdd78d9cb1b28d
MuMu <tim80031@hotmail.com>
2023-07-14 17:34:17 +0800

    ADDVM

--- prjEShopping/Controllers/SellerShipmentController.cs
+++ prjEShopping/Controllers/SellerShipmentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using prjEShopping.Models.EFModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +17,8 @@ namespace prjEShopping.Controllers
 
 		public ActionResult ShipmentList()
 		{
-            
+            AppDbContext db = new AppDbContext();
+
 			return View();
 		}
 	}
---
+++ prjEShopping/Models/ViewModels/SellerShipmentVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace prjEShopping.Models.ViewModels
+{
+    public class SellerShipmentVM
+    {
+        public int ShipmentNumber { get; set; }
+
+        public DateTime ShipmentDate { get; set; }
+
+        public int ShipmentStatusId { get; set; }
+    }
+}
