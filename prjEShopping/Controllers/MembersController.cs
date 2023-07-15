e6ec4c31e38341dd1dead1e33d2bc2cdedc7a9ee
SIH CI ZENG <aa51223tw28@gmail.com>
2023-07-15 19:33:52 +0800

    userlogin1

--- prjEShopping/Controllers/MembersController.cs
+++ prjEShopping/Controllers/MembersController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using prjEShopping.Models.EFModels;
+using prjEShopping.Models.Infra;
+using prjEShopping.Models.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace prjEShopping.Controllers
 {
@@ -12,6 +17,34 @@ namespace prjEShopping.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginVM vm)
+        {
+            if(ModelState.IsValid==false) return View(vm);//以便用戶能夠看到錯誤訊息和原始輸入值，並進行必要的修正
+
+            //驗證帳密的正確性
+            Result result
+
+
+        }
+
+        private Result VailLogin(LoginVM vm)
+        {
+            var db = new AppDbContext();
+            var member=db.Users.FirstOrDefault(x=>x.UserAccount==vm.UserAccount);
+
+            if (member == null) return Result.Fail("帳密有誤");
+
+            //會員資格要是AccessRightId==1(使用中)
+            var right = member.AccessRightId.ToString();
+
+            if (right != "1")
+            {
+                return Result.Fail("會員資格尚未確認");
+            }
+
+            var 
         }
     }
 }
---
+++ prjEShopping/Models/Infra/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjEShopping.Models.Infra
+{
+    public class Result
+    {
+        public bool IsSuccess { get;private set; }//private不能被改變value
+        public bool IsFail=>!IsSuccess;
+        public string ErrorMessage { get; private set; }
+        public static Result Success()=>new Result { IsSuccess = true, ErrorMessage = null };//static方法可以不要new
+        public static Result Fail(string errorMessage) => new Result { IsSuccess = false, ErrorMessage = errorMessage };
+    }
+}
