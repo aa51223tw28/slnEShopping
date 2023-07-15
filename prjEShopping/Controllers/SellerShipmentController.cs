fd58534aef2a2c274105f547d6bf2d3d4c5d65e2
MuMu <tim80031@hotmail.com>
2023-07-16 00:54:08 +0800

    購買明細版1

--- prjEShopping/Controllers/SellerShipmentController.cs
+++ prjEShopping/Controllers/SellerShipmentController.cs
@@ -40,13 +40,23 @@ namespace prjEShopping.Controllers
 
         public ActionResult ShipmentDetail(string ShipNum)
         {
+            //寄件明細
             var db = new AppDbContext();
             var data = db.ShipmentDetails.Where(x => x.ShipmentNumber == ShipNum).SingleOrDefault();
-            ViewBag.Smethod = db.ShippingMethods.Where(x => x.ShippingMethodId == data.ShippingMethodId).Select(x => x.ShippingMethodName);
-            ViewBag.Pmethod = db.PaymentMethods.Where(x => x.PaymentMethodId == data.PaymentMethodId).Select(x => x.PaymentMethodName);
+            ViewBag.Smethod = (db.ShippingMethods.Where(x => x.ShippingMethodId == data.ShippingMethodId).FirstOrDefault()).ShippingMethodName;
+            ViewBag.Pmethod = (db.PaymentMethods.Where(x => x.PaymentMethodId == data.PaymentMethodId).FirstOrDefault()).PaymentMethodName;
             ViewBag.Recriver = data.Receiver;
             ViewBag.RAddress = data.ReceiverAddress;
 
+            //購買人
+            var userorderid = db.Shipments.Where(x => x.ShipmentNumber == ShipNum).SingleOrDefault();
+
+            //拆開版
+            //var userid = db.Orders.Where(y => y.OrderId == userorderid.OrderId).FirstOrDefault().UserId;
+
+            //濃縮版
+			var username = db.Users.Where(x => x.UserId == (db.Orders.Where(y => y.OrderId == userorderid.OrderId).FirstOrDefault().UserId)).SingleOrDefault().UserName;
+            ViewBag.username = username;
 			return View();
         }
     }
