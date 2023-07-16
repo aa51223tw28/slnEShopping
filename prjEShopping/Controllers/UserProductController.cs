5dc2e36373b066ef1c1bfccdf2a369cef6df48b4
SIH CI ZENG <aa51223tw28@gmail.com>
2023-07-16 22:09:51 +0800

    product ing

--- prjEShopping/Controllers/UserProductController.cs
+++ prjEShopping/Controllers/UserProductController.cs
@@ -11,18 +11,32 @@ namespace prjEShopping.Controllers
     public class UserProductController : Controller
     {
         // GET: UserProduct
-        public ActionResult ProdutList()
+        public ActionResult UserProdutList()
         {
             var db = new AppDbContext();
             List<Product> product = db.Products.ToList();
             return View(product);
         }
 
-        public ActionResult SingleProduct(int productId)
+        public ActionResult UserSingleProduct(int productId)
         {            
             var db=new AppDbContext();
-            var product = db.Products.Where(x => x.ProductId == productId).FirstOrDefault();
-            return View(product);
+            List<UserProductIndexDto> datas;
+            datas=db.Products.ToList()
+                                .Select(x =>
+                                {
+                                    return new UserProductIndexDto()
+                                    {
+                                        ProductId = productId,
+                                        ProductName=x.ProductName,
+                                        ProductDescription=x.ProductDescription,
+                                        Price=(decimal)x.Price,
+                                        ProductImagePathOne=x.ProductImagePathOne,
+                                        ProductImagePathTwo=x.ProductImagePathTwo,
+                                        ProductImagePathThree=x.ProductImagePathThree,
+                                    };
+                                }).ToList();
+            return View(datas);
         }
     }
 }
