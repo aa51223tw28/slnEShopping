0ef5883ad0083175d9128e2c5fc356029e4a095a
SIH CI ZENG <aa51223tw28@gmail.com>
2023-07-16 21:17:50 +0800

    delete ProductIndexVM

--- prjEShopping/Models/DTOs/ProductIndexDto.cs
+++ prjEShopping/Models/DTOs/ProductIndexDto.cs
@@ -7,5 +7,14 @@ namespace prjEShopping.Models.DTOs
 {
     public class ProductIndexDto
     {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }//待確認
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ProductImagePathOne { get; set; }
+        public string ProductImagePathTwo { get; set; }
+        public string ProductImagePathThree { get; set; }
+        public int Stock { get; set; }
     }
 }
