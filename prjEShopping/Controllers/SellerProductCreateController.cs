33d34ef6dc22c7cce71b274755e6df33729e0538
MuMu <tim80031@hotmail.com>
2023-07-20 12:30:06 +0800

    主分類載入OK

---
+++ prjEShopping/Controllers/SellerProductCreateController.cs
@@ -0,0 +1,31 @@
+﻿using prjEShopping.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class SellerProductCreateController : Controller
+    {
+        // GET: SellerProductCreate
+        public ActionResult getCategoryName()
+        {
+            AppDbContext db = new AppDbContext();
+            var categorynames = db.ProductMainCategories.Select(x => x.CategoryName).Distinct();
+            return Json(categorynames,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult getSubcategoryName(string categoryname) 
+        {
+            AppDbContext db = new AppDbContext();
+            var subcategorynames = db.ProductMainCategories.Where(x => x.CategoryName == categoryname)
+                                .Join(db.ProductSubCategories, x => x.CategoryId, y => y.CategoryId, (x, y) => new
+                                {
+                                    subcategorynames = y.SubcategoryName
+                                });
+            return Json(subcategorynames);
+        }
+    }
+}
