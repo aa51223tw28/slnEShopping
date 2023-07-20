44ff2e794ff3133b771a6f3e2d3e922c076e3dfd
aa51223tw28 <aa51223tw28@gmail.com>
2023-07-20 16:54:48 +0800

    整理成usercartcontroller

---
+++ prjEShopping/Controllers/UserCartController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class UserCartController : Controller
+    {
+        // GET: UserCart
+                
+        [Authorize]
+        public ActionResult UserShoppingCart()//購物車頁面
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult UserCheckout()//結帳頁面
+        {
+            return View();
+        }
+        
+        [Authorize]
+        public ActionResult UserOrderDetail()//訂單詳情頁面
+        {
+            return View();
+        }
+    }
+ }
