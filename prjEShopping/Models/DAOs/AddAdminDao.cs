19490cef42308158636bf3f82ed994dd9bae993e
SieJH <sjh.jason@gmail.com>
2023-07-21 11:28:18 +0800

    7/21 密碼狀態確認

---
+++ prjEShopping/Models/DAOs/AddAdminDao.cs
@@ -0,0 +1,35 @@
+﻿using prjEShopping.Models.DTOs;
+using prjEShopping.Models.EFModels;
+using prjEShopping.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Web;
+
+namespace prjEShopping.Models.DAOs
+{
+    public class AddAdminDao
+    {
+        public void AddAdmin(AdminDto dto)
+        {
+            AppDbContext db = new AppDbContext();
+            var admin = new Admin()
+            {
+                AdminId = dto.AdminId,
+                AdminNumber = dto.AdminNumber,
+                PermissionsId = dto.PermissionsId,
+                AdminAccount = dto.AdminAccount,
+                AdminPassword = dto.AdminPassword,
+                Title = dto.Title,
+                AdminName = dto.AdminName,
+                Phone = dto.Phone,
+                DateOfHire = dto.DateOfHire,
+                JobStatus = dto.JobStatus
+            };
+
+            db.Admins.Add(admin);
+            db.SaveChanges();
+        }
+    }
+}
