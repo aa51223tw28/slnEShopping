b912e6bf5e2fd9eaec70fa0a0626087dd832b36e
SieJH <sjh.jason@gmail.com>
2023-07-21 15:00:13 +0800

    7/21 下午暫存

--- prjEShopping/Models/ViewModels/AdminVM.cs
+++ prjEShopping/Models/ViewModels/AdminVM.cs
@@ -15,33 +15,41 @@ namespace prjEShopping.Models.ViewModels
         public string AdminNumber { get; set; }
 
         [DisplayName("權限等級")]
+        [Required]
         public int? PermissionsId { get; set; }
 
         [DisplayName("帳號")]
         [StringLength(50)]
+        [Required]
         public string AdminAccount { get; set; }
 
         [DisplayName("密碼")]
         [StringLength(50)]
+        [Required]
         public string AdminPassword { get; set; }
 
         [DisplayName("職稱")]
         [StringLength(50)]
+        [Required]
         public string Title { get; set; }
 
         [DisplayName("姓名")]
         [StringLength(50)]
+        [Required]
         public string AdminName { get; set; }
 
         [DisplayName("電話")]
-        [StringLength(50)]
+        [StringLength(10)]
+        [Required]
         public string Phone { get; set; }
 
         [DisplayName("入職時間")]
+        [Required]
         public DateTime? DateOfHire { get; set; }
 
         [DisplayName("在職狀態")]
         [StringLength(50)]
+        [Required]
         public string JobStatus { get; set; }
     }
 }
