6a180468b1e77914bd43caf74a1af14c283055f5
SIH CI ZENG <aa51223tw28@gmail.com>
2023-07-23 11:27:52 +0800

    UserOrderVM OK

--- prjEShopping/Models/ViewModels/UserOrderVM.cs
+++ prjEShopping/Models/ViewModels/UserOrderVM.cs
@@ -5,7 +5,22 @@ using System.Web;
 
 namespace prjEShopping.Models.ViewModels
 {
-    public class UserOrderVM
+    public class UserOrderVM//長的跟UserShoppingCartVM很像待之後優化
     {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public int CouponId { get; set; }//先寫死
+        public DateTime OrderDate { get; set; }
+        public string OrderNumber { get; set; }
+        public int OrderDetailId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int SellerId { get; set; }
+        //public string SellerName { get; set; }
+        public int Quantity { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal CurrentSubTotal { get; set; }
+        public string ProductImagePathOne { get; set; }
+
     }
 }
