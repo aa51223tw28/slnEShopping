b13bd5da76dba964dd0ddc9d001816712474c688
allen <abamamatw@gmail.com>
2023-07-24 16:24:54 +0800

    0724合併

--- prjEShopping/Controllers/SellerRegisterController.cs
+++ prjEShopping/Controllers/SellerRegisterController.cs
@@ -1,4 +1,6 @@
-﻿using prjEShopping.Models.EFModels;
+﻿using prjEShopping.Models;
+using prjEShopping.Models.EFModels;
+using prjEShopping.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,20 @@ namespace prjEShopping.Controllers
 {
     public class SellerRegisterController : Controller
     {
+        public ActionResult List()
+        {
+            AppDbContext db = new AppDbContext();
+            var datas = from t in db.Sellers
+                        select t;
+            return View(datas);
+        }
+        public ActionResult List1()
+        {
+            AppDbContext db = new AppDbContext();
+            var datas = from t in db.Sellers
+                        select t;
+            return Json(datas);
+        }
         // GET: SellerRegister
         public ActionResult Create()
         {
@@ -21,6 +37,24 @@ namespace prjEShopping.Controllers
             db.Sellers.Add(s);
             db.SaveChanges();
             return RedirectToAction("List");
+            
         }
+
+
+        //public ActionResult Save()
+        //{
+        //    SellerRegisterVM s = new SellerRegisterVM();
+        //    s.GUINumber = Request.Form["txtGUINumber"];
+        //    s.SellerName = Request.Form["txtSellerName"];
+        //    s.StoreName = Request.Form["txtStoreName"];
+        //    s.SellerAccount = Request.Form["txtSellerAccount"];
+        //    s.SellerPassword = Request.Form["txtSellerPassword"];
+        //    s.Address = Request.Form["txtAddress"];
+        //    s.Phone = Request.Form["txtPhone"];
+        //    s.BankAccount = Request.Form["txtBankAccount"];
+        //    s.StoreIntro = Request.Form["txtStoreIntro"];
+        //     (new CFactory()).create(s);
+        //    return RedirectToAction("List");
+        //}
     }
 }
