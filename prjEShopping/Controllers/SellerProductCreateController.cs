ec854fa8988796139d49cda2b264572a7d988263
MuMu <tim80031@hotmail.com>
2023-07-24 18:35:54 +0800

    var productdetail = new ProductDetail()錯誤待改

--- prjEShopping/Controllers/SellerProductCreateController.cs
+++ prjEShopping/Controllers/SellerProductCreateController.cs
@@ -21,28 +21,37 @@ namespace prjEShopping.Controllers
 			return View();
 		}
 
-        //[HttpPost]
-        //public ActionResult ProductCreate(SellerProductCreateVM vm)
-        //{
-        //    var db = new AppDbContext();
-        //    var product = new Product()
-        //    {
-        //        ProductName = vm.ProductName,
-        //        ProductDescription = vm.ProductDescription,
-        //        Price = vm.Price,
-        //        BrandId = (db.Brands.Where(x => x.BrandName == vm.BrandName).SingleOrDefault()).BrandId,
-        //        ProductStatusId = 2,
-        //    };
-        //    db.Products.Add(product);
-        //    db.SaveChanges();
+        [HttpPost]
+        public ActionResult ProductCreate(SellerProductCreateVM vm)
+        {
+            var db = new AppDbContext();
+            //var product = new Product()
+            //{
+            //    ProductName = vm.ProductName,
+            //    ProductDescription = vm.ProductDescription,
+            //    Price = vm.Price,
+            //    BrandId = (db.Brands.Where(x => x.BrandName == vm.BrandName).SingleOrDefault()).BrandId,
+            //    ProductStatusId = 2,
+            //    SellerId = 1,
+            //};
+            //db.Products.Add(product);
+            //db.SaveChanges();
 
-        //    var productdetail = new ProductDetail()
-        //    {
-        //        CategoryId = (db.ProductMainCategories.Where(x => x.CategoryName == vm.CategoryName).SingleOrDefault()).CategoryId,
-        //        SubcategoryId = (db.ProductSubCategories.Where(x => x.SubcategoryName == vm.SubcategoryName).SingleOrDefault()).SubcategoryId,
-        //        OptionIdOne = db.ProductOptions.Where(x => x.SpecificationId == (db.ProductSpecifications.Where(y =>y.SpecificationName == vm.SpecificationName1).SingleOrDefault().SpecificationId).SingleOrDefault()).OptionId,
-        //    };
-        //    return RedirectToAction("Index","SellerMain");
-        //}
+            var productdetail = new ProductDetail()
+            {
+                ProductId = 25,
+                CategoryId = (db.ProductMainCategories.Where(x => x.CategoryName == vm.CategoryName).SingleOrDefault()).CategoryId,
+                SubcategoryId = (db.ProductSubCategories.Where(x => x.SubcategoryName == vm.SubcategoryName).SingleOrDefault()).SubcategoryId,
+                OptionIdOne = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName0 && x.OptionId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName0).SingleOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+                OptionIdTwo = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName1 && x.OptionId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName1).SingleOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+                OptionIdThree = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName2 && x.OptionId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName2).SingleOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+                OptionIdFour = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName3 && x.OptionId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName3).SingleOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+                OptionIdFive = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName4 && x.OptionId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName4).SingleOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+            };
+            db.ProductDetails.Add(productdetail);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "SellerMain");
+        }
     }
 }
