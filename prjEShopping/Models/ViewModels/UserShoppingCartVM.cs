eb92ca961e87397387970b2ac9036c1f46c4bb3b
aa51223tw28 <aa51223tw28@gmail.com>
2023-07-24 17:12:12 +0800

    合併成功j

--- prjEShopping/Models/ViewModels/UserShoppingCartVM.cs
+++ prjEShopping/Models/ViewModels/UserShoppingCartVM.cs
@@ -17,7 +17,8 @@ namespace prjEShopping.Models.ViewModels
         //public string SellerName { get; set; }
         public int Quantity { get; set; }
         public decimal SubTotal { get; set; }
-        
+        public decimal Price { get; set; }
+
         public string ProductImagePathOne { get; set; }
     }
 }
