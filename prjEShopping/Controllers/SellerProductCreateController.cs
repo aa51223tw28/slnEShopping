87f109405ab412cb657c21a250f79cb45ba6bc7a
MuMu <tim80031@hotmail.com>
2023-07-26 17:16:53 +0800

    資料庫更新0726

--- prjEShopping/Controllers/SellerProductCreateController.cs
+++ prjEShopping/Controllers/SellerProductCreateController.cs
@@ -37,19 +37,19 @@ namespace prjEShopping.Controllers
             db.Products.Add(product);
             db.SaveChanges();
 
-            var productdetail = new ProductDetail()
-            {
-                ProductId = 25,
-                CategoryId = (db.ProductMainCategories.Where(x => x.CategoryName == vm.CategoryName).SingleOrDefault()).CategoryId,
-                SubcategoryId = (db.ProductSubCategories.Where(x => x.SubcategoryName == vm.SubcategoryName).SingleOrDefault()).SubcategoryId,
-                OptionIdOne = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName0 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName0 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
-                OptionIdTwo = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName1 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName1 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
-                OptionIdThree = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName2 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName2 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
-                OptionIdFour = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName3 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName3 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
-                OptionIdFive = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName4 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName4 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
-            };
-            db.ProductDetails.Add(productdetail);
-            db.SaveChanges();
+            //var productdetail = new ProductDetail()
+            //{
+            //    ProductId = 25,
+            //    CategoryId = (db.ProductMainCategories.Where(x => x.CategoryName == vm.CategoryName).SingleOrDefault()).CategoryId,
+            //    SubcategoryId = (db.ProductSubCategories.Where(x => x.SubcategoryName == vm.SubcategoryName).SingleOrDefault()).SubcategoryId,
+            //    OptionIdOne = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName0 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName0 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+            //    OptionIdTwo = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName1 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName1 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+            //    OptionIdThree = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName2 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName2 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+            //    OptionIdFour = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName3 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName3 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+            //    OptionIdFive = (db.ProductOptions.Where(x => x.OptionName == vm.OptionName4 && x.SpecificationId == (db.ProductSpecifications.Where(y => y.SpecificationName == vm.SpecificationName4 && y.SubcategoryId == (db.ProductSubCategories.Where(z => z.SubcategoryName == vm.SubcategoryName).FirstOrDefault().SubcategoryId)).FirstOrDefault()).SpecificationId).SingleOrDefault().OptionId).ToString(),
+            //};
+            //db.ProductDetails.Add(productdetail);
+            //db.SaveChanges();
 
             return RedirectToAction("Index", "SellerMain");
         }
--- prjEShopping/Models/EFModels/Coupon.cs
+++ prjEShopping/Models/EFModels/Coupon.cs
@@ -10,52 +10,39 @@ namespace prjEShopping.Models.EFModels
     {
         public int CouponId { get; set; }
 
-        [StringLength(50)]
+        public int? SellerId { get; set; }
 
-        [Display(Name ="券號")]
+        [StringLength(50)]
         public string CouponNumber { get; set; }
 
-        [Display(Name = "券名")]
         [StringLength(50)]
         public string CouponName { get; set; }
 
-        [Display(Name = "說明")]
         [StringLength(50)]
         public string CouponDetails { get; set; }
 
-        [Display(Name = "數量")]
-        [RegularExpression("^[0-9]*$", ErrorMessage = "數量必須是數字")]
         public int? Quantity { get; set; }
 
-        [Display(Name = "使用條件")]
+        public int? ReceivedQuantity { get; set; }
+
         [StringLength(50)]
         public string CouponTerms { get; set; }
 
-        [Display(Name = "折扣類別")]
         [StringLength(50)]
         public string CouponType { get; set; }
 
-        [Display(Name = "折扣內容")]
         [StringLength(50)]
         public string Discount { get; set; }
 
-        [Display(Name = "使用範圍")]
         [StringLength(50)]
         public string Storewide { get; set; }
 
-        [Display(Name = "開始時間")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? StartTime { get; set; }
 
-        [Display(Name = "領取截止")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? ClaimDeadline { get; set; }
 
-        [Display(Name = "活動截止")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? EndTime { get; set; }
 
-        [Display(Name = "活動狀態")]
         [StringLength(50)]
         public string EventStatus { get; set; }
     }
