ba0254791dae27eb9836f990bf40472a8b69c903
SieJH <sjh.jason@gmail.com>
2023-07-26 23:34:26 +0800

    建立優惠券VM

---
+++ prjEShopping/Models/ViewModels/CouponVM.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace prjEShopping.Models.ViewModels
+{
+    public class CouponVM
+    {
+        public int CouponId { get; set; }
+
+        [StringLength(50)]
+
+        [Display(Name = "商家ID")]
+        public int? SellerId { get; set; }
+
+        [StringLength(50)]
+
+        [Display(Name = "券號")]
+        public string CouponNumber { get; set; }
+
+        [Display(Name = "券名")]
+        [StringLength(50)]
+        public string CouponName { get; set; }
+
+        [Display(Name = "說明")]
+        [StringLength(50)]
+        public string CouponDetails { get; set; }
+
+        [Display(Name = "數量")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "數量必須是數字")]
+        public int? Quantity { get; set; }
+
+        [Display(Name = "領取數量")]
+        public int? ReceivedQuantity { get; set; }
+
+        [Display(Name = "使用條件")]
+        [StringLength(50)]
+        public string CouponTerms { get; set; }
+
+        [Display(Name = "折扣類別")]
+        [StringLength(50)]
+        public string CouponType { get; set; }
+
+        [Display(Name = "折扣內容")]
+        [StringLength(50)]
+        public string Discount { get; set; }
+
+        [Display(Name = "使用範圍")]
+        [StringLength(50)]
+        public string Storewide { get; set; }
+
+        [Display(Name = "開始時間")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? StartTime { get; set; }
+
+        [Display(Name = "領取截止")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? ClaimDeadline { get; set; }
+
+        [Display(Name = "活動截止")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? EndTime { get; set; }
+
+        [Display(Name = "活動狀態")]
+        [StringLength(50)]
+        public string EventStatus { get; set; }
+    }
+}
