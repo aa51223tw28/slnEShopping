726a86a94c4e494fd41f43417609ac8e995aafed
allen <abamamatw@gmail.com>
2023-07-28 15:18:33 +0800

    create photo ok

--- prjEShopping/Controllers/SellerRegisterController.cs
+++ prjEShopping/Controllers/SellerRegisterController.cs
@@ -3,6 +3,7 @@ using prjEShopping.Models.EFModels;
 using prjEShopping.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Web;
@@ -27,10 +28,35 @@ namespace prjEShopping.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Create(Seller s)
+        public ActionResult Create(Seller s, HttpPostedFileBase photo)
         {
-            //var itemship = new AppDbContext().ShippingMethodId.Where(x => x.ShippingMethodId == shipid).Select(x => x.ShippingMethodId).FirstOrDefault();
-            //var itempay = new AppDbContext().PaymentMethodId.Where(x => x.PaymentMethodId == payid).Select(x => x.PaymentMethodId).FirstOrDefault();
+            if (photo != null && photo.ContentLength > 0)
+            {
+                // 確認有上傳圖片
+                // 獲取文件名和副檔名
+                var fileName = Path.GetFileName(photo.FileName);
+                var fileExtension = Path.GetExtension(fileName);
+
+                // 生成唯一的文件名，以避免重複
+                var uniqueFileName = $"{Guid.NewGuid()}{fileExtension}";
+
+                // 獲取伺服器上的路徑
+                var serverPath = Server.MapPath("~/Uploads/"); // 這裡假設保存到名為 "Uploads" 的資料夾中
+
+                // 確保資料夾存在
+                if (!Directory.Exists(serverPath))
+                {
+                    Directory.CreateDirectory(serverPath);
+                }
+
+                // 保存圖片到伺服器
+                var fullPath = Path.Combine(serverPath, uniqueFileName);
+                photo.SaveAs(fullPath);
+
+                // 將圖片路徑保存到資料庫中
+                s.SellerImagePath = uniqueFileName;
+            }
+
             if (String.IsNullOrEmpty(s.GUINumber))
             {
                 TempData["Fail"] = "統編為必填欄位！";
