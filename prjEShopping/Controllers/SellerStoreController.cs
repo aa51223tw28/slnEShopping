735f1e23e41a55fe3666a4a50dd305777cf442eb
MuMu <tim80031@hotmail.com>
2023-07-30 02:15:18 +0800

    加入店家首頁

---
+++ prjEShopping/Controllers/SellerStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class SellerStoreController : Controller
+    {
+        // GET: SellerStore
+        public ActionResult StoreMain(int id)
+        {
+            return View();
+        }
+    }
+}
