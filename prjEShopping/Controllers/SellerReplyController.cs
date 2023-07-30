07585d11cc29dbec73753a85f4ebf35daec63ac1
abamamatw <abamamatw@gmail.com>
2023-07-30 20:49:28 +0800

    +controller

---
+++ prjEShopping/Controllers/SellerReplyController.cs
@@ -0,0 +1,23 @@
+﻿using prjEShopping.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class SellerReplyController : Controller
+    {
+        // GET: SellerReply
+        private readonly AppDbContext db = new AppDbContext();
+
+        [HttpPost]
+        public ActionResult AddReply(int commentId, string replyText)
+        {
+            // Add reply logic here
+            // ...
+            return RedirectToAction("Index", "BuyerFeedback"/*, new { sellerId = /*sellerIdFromComment*/ );
+        }
+    }
+}
