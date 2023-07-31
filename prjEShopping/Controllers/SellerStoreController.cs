2eee84249ee50db8976505c75f2534ab8ac0def7
MuMu <tim80031@hotmail.com>
2023-07-31 15:22:11 +0800

    商家簡介可傳(帶改評價、星數、收藏數)

--- prjEShopping/Controllers/SellerStoreController.cs
+++ prjEShopping/Controllers/SellerStoreController.cs
@@ -13,6 +13,13 @@ namespace prjEShopping.Controllers
         public ActionResult StoreMain()
         {
             var db = new AppDbContext();
+            ViewBag.AllRatingStar = 29;
+            ViewBag.RatingCount = 7;
+            ViewBag.AvgRating = ((double)(ViewBag.AllRatingStar)/ (ViewBag.RatingCount)).ToString("F1");
+            ViewBag.Storename = db.Sellers.FirstOrDefault(x => x.SellerId == 1).SellerName;
+            ViewBag.StoreIntro = db.Sellers.FirstOrDefault(x => x.SellerId == 1).StoreIntro;
+            ViewBag.CollectedCount = 0;
+            ViewBag.StoreImage = db.Sellers.FirstOrDefault(x => x.SellerId == 1).SellerImagePath;
             var products = db.Products.Where(x => x.SellerId == 1).ToList();
             return View(products);
         }
