9829d9b16bbcf2dd31dccfeaa8c5d5f52b0b4ebc
SieJH <sjh.jason@gmail.com>
2023-08-01 15:38:35 +0800

    點數購買調整

--- prjEShopping/Controllers/ADPointsController.cs
+++ prjEShopping/Controllers/ADPointsController.cs
@@ -16,13 +16,35 @@ namespace prjEShopping.Controllers
         private AppDbContext db = new AppDbContext();
 
         // GET: ADPoints
-        public ActionResult Index()
+        public ActionResult List()
         {
-            return View(db.ADPoints.ToList());
+            var model= db.ADPoints.ADPoint2VM().ToList();
+            return View(model);
         }
 
-        // GET: ADPoints/Details/5
-        public ActionResult Details(int? id)
+        [HttpPost]
+        public ActionResult List(string search)
+        {
+            int sellerId;
+            if (int.TryParse(search, out sellerId))
+            {
+                var model = db.ADPoints.Where(x => x.SellerId == sellerId).ADPoint2VM().ToList();
+                return View(model);
+            }
+
+            else
+            {
+                if (search != null)
+                {
+                    var model = db.ADPoints.Where(x => x.GUINumber == search).ADPoint2VM().ToList();
+                    return View(model);
+                }
+                return View("List");
+            }
+        }
+
+            // GET: ADPoints/Details/5
+            public ActionResult Details(int? id)
         {
             if (id == null)
             {
@@ -36,91 +58,34 @@ namespace prjEShopping.Controllers
             return View(aDPoint);
         }
 
-        // GET: ADPoints/Create
-        public ActionResult Create(int? sellerId)
+        // GET: ADPoints/BuyPoints
+        public ActionResult BuyPoints(int? sellerId)
         {
-            sellerId = 6;
-            var model = db.ADPoints.ADPoint2VM().FirstOrDefault();
+            sellerId = 2;
+            var model = db.ADPoints.Where(x=>x.SellerId==sellerId).ADPoint2VM().FirstOrDefault();
             ViewBag.List = db.ADPoints.Where(s=>s.SellerId==sellerId).OrderByDescending(d=>d.PurchaseTime).ToList();
             ViewBag.SellerPoint=db.Sellers.Where(s => s.SellerId == sellerId).Select(p=>p.ADPoints).FirstOrDefault();
             return View(model);
         }
 
-        // POST: ADPoints/Create
+        // POST: ADPoints/BuyPoints
         // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
         // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ADPointId,SellerId,GUINumber,PurchaseTime,ADPoints,PaymentStatus")] ADPointVM vm)
+        public ActionResult BuyPoints([Bind(Include = "ADPointId,SellerId,GUINumber,PurchaseTime,ADPoints,PaymentStatus")] ADPointVM vm)
         {
-            var sellerId = 6;
+
             if (ModelState.IsValid)
             {            
-                var Seller=db.Sellers.FirstOrDefault(x=>x.SellerId == sellerId);
+                var Seller=db.Sellers.FirstOrDefault(x=>x.SellerId == vm.SellerId);
                 Seller.ADPoints = Seller.ADPoints+ADPointChange.VM2ADPoint(vm).ADPoints;             
                 db.ADPoints.Add(ADPointChange.VM2ADPoint(vm));
                 db.SaveChanges();
-                return RedirectToAction("Create");
+                return RedirectToAction("BuyPoints");
             }
 
             return View(vm);
-        }
-
-        // GET: ADPoints/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ADPoint aDPoint = db.ADPoints.Find(id);
-            if (aDPoint == null)
-            {
-                return HttpNotFound();
-            }
-            return View(aDPoint);
-        }
-
-        // POST: ADPoints/Edit/5
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ADPointId,SellerId,GUINumber,PurchaseTime,ADPoints,PaymentStatus")] ADPoint aDPoint)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(aDPoint).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(aDPoint);
-        }
-
-        // GET: ADPoints/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ADPoint aDPoint = db.ADPoints.Find(id);
-            if (aDPoint == null)
-            {
-                return HttpNotFound();
-            }
-            return View(aDPoint);
-        }
-
-        // POST: ADPoints/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            ADPoint aDPoint = db.ADPoints.Find(id);
-            db.ADPoints.Remove(aDPoint);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
--- prjEShopping/Models/ViewModels/ADPointVM.cs
+++ prjEShopping/Models/ViewModels/ADPointVM.cs
@@ -13,16 +13,21 @@ namespace prjEShopping.Models.ViewModels
         
             public int ADPointId { get; set; }
 
+            [DisplayName("商家Id")]
             public int? SellerId { get; set; }
 
+            [DisplayName("商家統編")]
             [StringLength(50)]
             public string GUINumber { get; set; }
 
-            public DateTime? PurchaseTime { get; set; }
+             [DisplayName("購買時間")]
+             [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+             public DateTime? PurchaseTime { get; set; }
 
             [DisplayName("廣告點數")]
             public int? ADPoints { get; set; }
 
+            [DisplayName("廣告點數")]
             [StringLength(50)]
             public string PaymentStatus { get; set; }
        
@@ -31,7 +36,7 @@ namespace prjEShopping.Models.ViewModels
     public static class ADPointChange
     {
         public static ADPoint VM2ADPoint(ADPointVM vm)
-        {
+        {          
             return new ADPoint
             {
                 ADPointId = vm.ADPointId,
@@ -45,11 +50,13 @@ namespace prjEShopping.Models.ViewModels
 
         public static ADPointVM ADPoint2VM(ADPoint point)
         {
+            var db = new AppDbContext();
+            var seller=db.Sellers.Where(x=>x.SellerId==point.SellerId).FirstOrDefault();
             return new ADPointVM
             {
                 ADPointId = point.ADPointId,
-                SellerId = point.SellerId,
-                GUINumber = point.GUINumber,
+                SellerId = seller.SellerId,
+                GUINumber = seller.GUINumber,
                 PurchaseTime = point.PurchaseTime,
                 ADPoints = point.ADPoints,
                 PaymentStatus = point.PaymentStatus,
