921f6c954303904a4422c077cf82536d76390fb4
allen <abamamatw@gmail.com>
2023-08-02 17:33:28 +0800

    test comment

---
+++ prjEShopping/Controllers/UserTestCommentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class UserTestCommentController : Controller
+    {
+        // GET: UserTestComment
+        public ActionResult Comment()
+        {
+            return View();
+        }
+    }
+}
