df88bf5dffc08b20a0634e86fdfdabead9093144
SIH CI ZENG <aa51223tw28@gmail.com>
2023-08-03 23:34:03 +0800

    UserFollowProduct頁面初步ok差再把追蹤移掉

--- prjEShopping/Controllers/UserFollowController.cs
+++ prjEShopping/Controllers/UserFollowController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using prjEShopping.Models.EFModels;
+using prjEShopping.Models.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,7 +22,34 @@ namespace prjEShopping.Controllers
         [Authorize]
         public ActionResult UserFollowProduct()
         {
-            return View();
+            var customerAccount = User.Identity.Name;
+
+            var db = new AppDbContext();
+            var userid = db.Users.Where(x => x.UserAccount == customerAccount).Select(x => x.UserId).FirstOrDefault();
+
+            List<UserFollowProductVM> datas= new List<UserFollowProductVM>();
+
+            var trackproduct = db.TrackProducts.Where(x => x.UserId == userid)
+                                                .Join(db.Products, x => x.ProductId, y => y.ProductId, (x, y) => new
+                                                {
+                                                    ProductId=x.ProductId,
+                                                    ProductName=y.ProductName,
+                                                    Price=y.Price,
+                                                    ProductImagePathOne=y.ProductImagePathOne,
+                                                    TrackProductId=x.TrackProductId,
+                                                }).ToList();
+
+            datas= trackproduct.Select(x=>new UserFollowProductVM
+            {
+                ProductId=(int)x.ProductId,
+                ProductName=x.ProductName,
+                Price=(decimal)x.Price,
+                ProductImagePathOne=x.ProductImagePathOne,
+                TrackProductId=x.TrackProductId,
+            }).ToList();
+
+
+            return View(datas);
         }
     }
 }
---
+++ prjEShopping/Models/ViewModels/UserFollowProductVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjEShopping.Models.ViewModels
+{
+    public class UserFollowProductVM
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }       
+        public decimal Price { get; set; }
+        public string ProductImagePathOne { get; set; }
+        public int TrackProductId { get; set; }
+
+    }
+}
