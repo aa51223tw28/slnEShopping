ffadad47069a02d52dcbcc14f98ff7f97defe063
MuMu <tim80031@hotmail.com>
2023-08-06 22:58:32 +0800

    活動Controller

---
+++ prjEShopping/Controllers/SellerActivitiesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class SellerActivitiesController : Controller
+    {
+        // GET: SellerActivities
+        public ActionResult ADList()
+        {
+            return View();
+        }
+
+        public ActionResult ADCreate()
+        {
+            return View();
+        }
+
+        public ActionResult CouponList()
+        {
+            return View();
+        }
+
+        public ActionResult CouponCreate()
+        {
+            return View();
+        }
+
+        public ActionResult MyADList()
+        {
+            return View();
+        }
+
+        public ActionResult MyADCreate()
+        {
+            return View();
+        }
+
+    }
+}
