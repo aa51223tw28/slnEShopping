51396abcee23f17ec4df82869f6b572c1b128e48
SieJH <sjh.jason@gmail.com>
2023-08-08 11:15:05 +0800

    基本回信

--- prjEShopping/Controllers/SupportController.cs
+++ prjEShopping/Controllers/SupportController.cs
@@ -1,6 +1,8 @@
 ﻿using prjEShopping.Models.EFModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,7 +16,7 @@ namespace prjEShopping.Controllers
         public ActionResult Index()
         {
             var userid = 1;
-            var model=db.Supports.Where(x=>x.UserId==userid).ToList();
+            var model = db.Supports.Where(x => x.UserId == userid).ToList();
             return View(model);
         }
 
@@ -29,17 +31,17 @@ namespace prjEShopping.Controllers
         [HttpPost]
         public ActionResult CSEmail(Support s)
         {
-            var sup=new Support();
+            var sup = new Support();
             sup = s;
             db.Supports.Add(sup);
             db.SaveChanges();
-            
+
             return RedirectToAction("Index");
         }
 
         public ActionResult CSEmailDetails(int? id)
         {
-            var model = db.Supports.FirstOrDefault(x=>x.SupportId==id);
+            var model = db.Supports.FirstOrDefault(x => x.SupportId == id);
             return View(model);
         }
 
@@ -47,9 +49,45 @@ namespace prjEShopping.Controllers
         {
             var userid = 1;
             var sup = new Support();
-            ViewBag.Userid=userid;
-            ViewBag.Support =sup;
+            ViewBag.Userid = userid;
+            ViewBag.Support = sup;
             return View();
+        }
+
+        //Customer Service System CSS
+        public ActionResult CSSIndex()
+        {
+            return View();
+        }
+
+        public ActionResult CSSNewMailList()
+        {
+            var model = db.Supports.Where(x => x.SupportStatus == "1").ToList();
+            return View(model);
+        }
+
+        public ActionResult CSSNewMailReplay(int? id)
+        {
+            var model = db.Supports.FirstOrDefault(x => x.SupportId == id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult CSSNewMailReplay(SupportReplay r)
+        {
+            var replay=new SupportReplay();
+            replay = r;
+            replay.AdminId =1;
+            var s=db.Supports.FirstOrDefault(x=>x.SupportId==r.SupportId);
+            if (s != null)
+            {
+                s.SupportStatus = "2";
+                db.Entry(s).State = EntityState.Modified;
+                db.SupportReplaies.Add(replay);
+                db.SaveChanges();
+                return RedirectToAction("CSSIndex");
+            }
+            return View(r);
         }
     }
 }
