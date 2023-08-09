9fa6e043b000c78605ef5faa9a7771800513da2a
SieJH <sjh.jason@gmail.com>
2023-08-09 20:45:19 +0800

    Seller暫存

---
+++ prjEShopping/Controllers/AdminSellersController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using prjEShopping.Models.EFModels;
+using prjEShopping.Models.ViewModels;
+
+namespace prjEShopping.Controllers
+{
+    public class AdminSellersController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: AdminSellers
+        public ActionResult Index()
+        {
+            var model = AdminSellerChange.AdminSeller2VM(db.Sellers);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult ChangeAccessRightId(int id, int AccessRightId)
+        {
+            var seller = db.Sellers.Find(id); 
+            seller.AccessRightId = AccessRightId;
+            db.SaveChanges();
+
+            // Return the new status
+            return Json(new { newAccessRightId = AccessRightId });
+        }
+
+        // GET: AdminSellers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Sellers.Find(id);
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // GET: AdminSellers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminSellers/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SellerId,SellerName,StoreName,SellerAccount,SellerPassword,SellerPasswordSalt,Phone,Address,GUINumber,StoreIntro,AccessRightId,BankAccount,PaymentMethodId,ShippingMethodId,ADPoints,SellerImagePath,Role,EmailCheck")] Seller seller)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sellers.Add(seller);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seller);
+        }
+
+        // GET: AdminSellers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Sellers.Find(id);
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // POST: AdminSellers/Edit/5
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SellerId,SellerName,StoreName,SellerAccount,SellerPassword,SellerPasswordSalt,Phone,Address,GUINumber,StoreIntro,AccessRightId,BankAccount,PaymentMethodId,ShippingMethodId,ADPoints,SellerImagePath,Role,EmailCheck")] Seller seller)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seller).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seller);
+        }
+
+        // GET: AdminSellers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Sellers.Find(id);
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // POST: AdminSellers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seller seller = db.Sellers.Find(id);
+            db.Sellers.Remove(seller);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ prjEShopping/Models/ViewModels/AdminSellerVM.cs
@@ -0,0 +1,101 @@
+﻿using prjEShopping.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace prjEShopping.Models.ViewModels
+{
+    public class AdminSellerVM
+    {
+        public int SellerId { get; set; }
+
+        [StringLength(50)]
+        public string SellerName { get; set; }
+
+        [StringLength(50)]
+        public string StoreName { get; set; }
+
+        [StringLength(50)]
+        public string SellerAccount { get; set; }
+
+        [StringLength(100)]
+        public string SellerPassword { get; set; }
+
+        [StringLength(100)]
+        public string SellerPasswordSalt { get; set; }
+
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        public string GUINumber { get; set; }
+
+        [StringLength(4000)]
+        public string StoreIntro { get; set; }
+
+        public int? AccessRightId { get; set; }
+
+        [StringLength(50)]
+        public string BankAccount { get; set; }
+
+        public int? PaymentMethodId { get; set; }
+
+        public int? ShippingMethodId { get; set; }
+
+        public int? ADPoints { get; set; }
+
+        [StringLength(50)]
+        public string SellerImagePath { get; set; }
+
+        [StringLength(50)]
+        public string Role { get; set; }
+
+        [StringLength(100)]
+        public string EmailCheck { get; set; }
+    }
+
+    public static class AdminSellerChange
+    {
+        public static AdminSellerVM AdminSeller2VM(Seller s)
+        {
+            return new AdminSellerVM
+            {
+                SellerId=s.SellerId,
+                SellerName=s.SellerName,
+                StoreName=s.StoreName,
+                SellerAccount=s.SellerAccount,
+                SellerPassword=s.SellerPassword,
+                SellerPasswordSalt=s.SellerPasswordSalt,
+                Phone=s.Phone,
+                Address=s.Address,
+                GUINumber=s.GUINumber,
+                StoreIntro=s.StoreIntro,
+                AccessRightId=s.AccessRightId,
+                BankAccount=s.BankAccount,
+                PaymentMethodId=s.PaymentMethodId,
+                ShippingMethodId=s.ShippingMethodId,
+                ADPoints=s.ADPoints,
+                SellerImagePath=s.SellerImagePath,
+                Role=s.Role,
+                EmailCheck=s.EmailCheck,
+            };
+        }
+
+        public static List<AdminSellerVM> AdminSeller2VM(this IEnumerable<Seller> source)
+        {
+            if (source == null || source.Count() == 0)
+            {
+                return Enumerable.Empty<AdminSellerVM>().ToList();
+            }
+
+            return source.Select(s => AdminSeller2VM(s)).ToList();
+        }
+    }
+}
