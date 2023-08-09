c19780f6bf36c24ede45f9ba58a6b6ce2eec3e80
SIH CI ZENG <aa51223tw28@gmail.com>
2023-08-09 23:28:30 +0800

    金流待從資料庫傳資料

---
+++ prjEShopping/Controllers/UserECpayController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class UserECpayController : Controller
+    {
+        // GET: UserECpay
+        public ActionResult ToECpay()
+        {
+            //要從資料庫撈資料傳去綠界
+            var orderId = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 20);
+            //需填入你的網址
+            var website = $"https://localhost:44388/";
+            var order = new Dictionary<string, string>
+            {
+                //特店交易編號
+                { "MerchantTradeNo",  orderId},
+                
+                //特店交易時間
+                { "MerchantTradeDate",  DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")},
+                
+                //交易金額
+                { "TotalAmount",  "100"},
+                
+                //交易描述
+                { "TradeDesc",  "無"},
+                
+                //商品名稱
+                { "ItemName",  "測試商品"},
+                
+                //允許繳費有效天數
+                { "ExpireDate",  "3"},
+                
+                { "CustomField1",  ""},
+                { "CustomField2",  ""},
+                { "CustomField3",  ""},
+                { "CustomField4",  ""},
+               
+                //綠界回傳付款資訊的至 此URL
+                { "ReturnURL",  $"{website}api/Ecpay/AddPayInfo"},//目前沒用到
+                
+                //使用者於綠界 付款完成後，綠界將會轉址至 此URL
+                { "OrderResultURL", $"{website}UserECpay/PayInfo/{orderId}"},
+                
+                //付款方式為 ATM 時，當使用者於綠界操作結束時，綠界回傳 虛擬帳號資訊至 此URL
+                { "PaymentInfoURL",  $"{website}/api/Ecpay/AddAccountInfo"},//目前沒用到
+                
+                //付款方式為 ATM 時，當使用者於綠界操作結束時，綠界會轉址至 此URL。
+                { "ClientRedirectURL",  $"{website}/Home/AccountInfo/{orderId}"},//目前沒用到
+                
+                //特店編號， 2000132 測試綠界編號
+                { "MerchantID",  "2000132"},
+               
+                //忽略付款方式
+                { "IgnorePayment",  "GooglePay#WebATM#CVS#BARCODE"},
+                
+                //交易類型 固定填入 aio
+                { "PaymentType",  "aio"},
+                
+                //選擇預設付款方式 固定填入 ALL
+                { "ChoosePayment",  "ALL"},
+                
+                //CheckMacValue 加密類型 固定填入 1 (SHA256)
+                { "EncryptType",  "1"},
+            };
+            //檢查碼
+            order["CheckMacValue"] = GetCheckMacValue(order);
+
+            return View(order);
+        }
+
+        private string GetCheckMacValue(Dictionary<string, string> order)
+        {
+            var param = order.Keys.OrderBy(x => x).Select(key => key + "=" + order[key]).ToList();
+
+            var checkValue = string.Join("&", param);
+
+            //測試用的 HashKey
+            var hashKey = "5294y06JbISpM5x9";
+
+            //測試用的 HashIV
+            var HashIV = "v77hoKGq4kWxNNIS";
+
+            checkValue = $"HashKey={hashKey}" + "&" + checkValue + $"&HashIV={HashIV}";
+
+            checkValue = HttpUtility.UrlEncode(checkValue).ToLower();
+
+            checkValue = GetSHA256(checkValue);
+
+            return checkValue.ToUpper();
+        }
+        private string GetSHA256(string value)
+        {
+            var result = new StringBuilder();
+            var sha256 = SHA256Managed.Create();
+            var bts = Encoding.UTF8.GetBytes(value);
+            var hash = sha256.ComputeHash(bts);
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+
+            return result.ToString();
+        }
+
+        [HttpPost]
+        public ActionResult PayInfo(string id)
+        {            
+            return View();
+        }
+    }
+}
