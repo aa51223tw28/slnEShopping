8aec82ef5d3b3174344b4e9fa3acf4f7e613ec78
allen <abamamatw@gmail.com>
2023-08-09 15:52:03 +0800

    機器人OK

---
+++ prjEShopping/Controllers/RobotController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class RobotController : Controller
+    {
+        // GET: Robot
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
