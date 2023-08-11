2759ef21ac97e22be380f2b376f731e4112b5991
SieJH <sjh.jason@gmail.com>
2023-08-11 16:53:18 +0800

    新的客服列表

---
+++ prjEShopping/Controllers/Support2Controller.cs
@@ -0,0 +1,24 @@
+﻿using prjEShopping.Models.EFModels;
+using prjEShopping.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class Support2Controller : Controller
+    {
+        AppDbContext db = new AppDbContext();
+        // GET: Support2
+        // CSS客服端
+        // CS 客戶端
+        public ActionResult CSSList()
+        {
+            var model = SupportChange.Support2VM(db.Supports);
+            
+            return View(model);
+        }
+    }
+}
--- prjEShopping/Models/ViewModels/CombinedSupportsVM.cs
+++ prjEShopping/Models/ViewModels/CombinedSupportsVM.cs
@@ -18,6 +18,13 @@ namespace prjEShopping.Models.ViewModels
     {
         public int SupportId { get; set; }
 
+        [StringLength(50)]
+        public string SupportNumber { get; set; }
+
+        public int? AdminId { get; set; }
+
+        public int? SellerId { get; set; }
+
         public int? UserId { get; set; }
 
         public int? ProductId { get; set; }
@@ -35,6 +42,9 @@ namespace prjEShopping.Models.ViewModels
 
         [StringLength(50)]
         public string SupportStatus { get; set; }
+
+        [StringLength(100)]
+        public string ImageLink { get; set; }
     }
 
     public partial class SupportReplayVM
@@ -44,6 +54,10 @@ namespace prjEShopping.Models.ViewModels
         public int? SupportId { get; set; }
 
         public int? AdminId { get; set; }
+
+        public int? SellerId { get; set; }
+
+        public int? UserId { get; set; }
 
         [StringLength(50)]
         public string ReplayTitle { get; set; }
@@ -55,5 +69,39 @@ namespace prjEShopping.Models.ViewModels
 
         [StringLength(50)]
         public string SupportReplayStatus { get; set; }
+
+        [StringLength(100)]
+        public string ImageLink { get; set; }
     }
-}
+
+
+    public static class SupportChange
+    {
+        public static SupportVM Support2VM(Support s)
+        {
+            return new SupportVM
+            {
+                SupportId = s.SupportId,
+                SupportNumber = s.SupportNumber,
+                AdminId = s.AdminId,
+                SellerId = s.SellerId,
+                UserId = s.UserId,
+                ProductId = s.ProductId,
+                SupportType = s.SupportType,
+                SupportTitle = s.SupportTitle,
+                SupportText = s.SupportText,
+                ReceivedTime = s.ReceivedTime,
+                SupportStatus = s.SupportStatus,
+                ImageLink = s.ImageLink,
+            };
+        }
+            public static List<SupportVM> Support2VM(this IEnumerable<Support> source)
+            {
+                if (source == null || source.Count() == 0)
+                {
+                    return Enumerable.Empty<SupportVM>().ToList();
+                }
+                return source.Select(s => Support2VM(s)).ToList();
+            }
+        }
+    }
