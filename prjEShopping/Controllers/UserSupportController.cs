4a1430c265fb44cb9029672c15702470091af7ee
SIH CI ZENG <aa51223tw28@gmail.com>
2023-08-13 13:44:24 +0800

    暫存

--- prjEShopping/Controllers/UserSupportController.cs
+++ prjEShopping/Controllers/UserSupportController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using prjEShopping.Models.EFModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static prjEShopping.Controllers.testproductsupportController;
 
 namespace prjEShopping.Controllers
 {
@@ -11,7 +13,16 @@ namespace prjEShopping.Controllers
         // GET: UserSupport
         public ActionResult UserCSList()
         {
-            return View();
+            var db = new AppDbContext();
+            var customerAccount = User.Identity.Name;
+
+            //找userid
+            var userid = db.Users.Where(x => x.UserAccount == customerAccount).Select(x => x.UserId).FirstOrDefault();
+
+            ViewBag.UserId = userid;
+            var s = db.Supports.Where(x => x.UserId == userid).ToList();
+            var model = SupportChange.Support2VM(s);
+            return View(model);            
         }
     }
 }
