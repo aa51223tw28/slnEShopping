0032aec46a127bcf13cf69848e3ff2e8f79c5d50
SIH CI ZENG <aa51223tw28@gmail.com>
2023-08-13 13:11:42 +0800

    暫存

---
+++ prjEShopping/Controllers/UserSupportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class UserSupportController : Controller
+    {
+        // GET: UserSupport
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
