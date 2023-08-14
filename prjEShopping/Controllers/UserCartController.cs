7d66696901ab3f9fe1264fb0f3423c968410a3f1
SIH CI ZENG <aa51223tw28@gmail.com>
2023-08-15 00:55:12 +0800

    0815存

--- prjEShopping/Controllers/UserCartController.cs
+++ prjEShopping/Controllers/UserCartController.cs
@@ -1071,7 +1071,7 @@ namespace prjEShopping.Controllers
 
             return result.ToString();
         }
-        
+        [Authorize]
         public ActionResult PayInfo()
         {
             var customerAccount = User.Identity.Name;
