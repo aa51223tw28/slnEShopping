21e65a7b9ac2692d21dbcff1993c2941201282ad
SieJH <sjh.jason@gmail.com>
2023-08-15 14:37:38 +0800

    商家基本附加功能完成

--- prjEShopping/Controllers/SellerActivitiesController.cs
+++ prjEShopping/Controllers/SellerActivitiesController.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace prjEShopping.Controllers
 {
@@ -29,12 +30,13 @@ namespace prjEShopping.Controllers
         }
 
         public ActionResult ADCreate(int adproductId)
-        { //廣告-2 購買廣告確認頁面
+        { //廣告-2-1 購買廣告確認頁面
             int? sellerId = Session["SellerId"] as int?;
             if (sellerId == null)
-            {
-                sellerId = 1;
-            }
+            { return RedirectToAction("Login", "SellerLogin"); }
+            else
+            { sellerId = (int)Session["SellerId"]; }
+
             var model = db.ADProducts.Where(a => a.ADProductId == adproductId).ADProduct2VM().FirstOrDefault();
 
             ViewBag.SellerId = sellerId;
@@ -45,7 +47,7 @@ namespace prjEShopping.Controllers
 
         [HttpPost]
         public ActionResult ADCreate(ADProductVM vm, int SellerId)
-        {
+        {//廣告-2-2 購買廣告資料庫紀錄
             if (ModelState.IsValid)
             {
                 var ad = db.ADProducts.Where(x => x.ADProductId == vm.ADProductId).FirstOrDefault();
@@ -93,7 +95,7 @@ namespace prjEShopping.Controllers
         }
         [HttpPost]
         public JsonResult UploadImage(HttpPostedFileBase ADImage)
-        {
+        {//廣告-2-3 購買廣告存圖
             if (ADImage != null && ADImage.ContentLength > 0)
             {
                 string fileName = Path.GetFileNameWithoutExtension(ADImage.FileName);
@@ -121,12 +123,12 @@ namespace prjEShopping.Controllers
             }
         }
         public ActionResult ADOwner() 
-        {
+        {//廣告-3 擁有廣告列表
             int? sellerId = Session["SellerId"] as int?;
             if (sellerId == null)
-            {
-                sellerId = 1;
-            }
+            { return RedirectToAction("Login", "SellerLogin"); }
+            else
+            { sellerId = (int)Session["SellerId"]; }
 
             var sellerAdProductIds = db.SellersADs
                            .Where(x => x.SellerId == sellerId)
@@ -142,17 +144,119 @@ namespace prjEShopping.Controllers
        
         public ActionResult CouponList()
         {
-            return View();
+            int? sellerId = Session["SellerId"] as int?;
+            if (sellerId == null)
+            {
+                sellerId = 1;
+            }
+                var model = db.Coupons.Where(x => x.SellerId == sellerId).Coupon2VM();
+            var path=db.Sellers.Where(x => x.SellerId == sellerId).Select(p=>p.SellerImagePath).FirstOrDefault();
+            ViewBag.ImagePath=path;
+                return View(model);
         }
 
-        public ActionResult CouponCreate()
+        public ActionResult CouponCreate(int? SellerId)
         {
+            int? sellerId = Session["SellerId"] as int?;
+            if (sellerId == null)
+            { return RedirectToAction("Login", "SellerLogin"); }
+            else
+            { sellerId = (int)Session["SellerId"]; }
+
+            SellerId = sellerId;
+            List<string> CouponsNums = db.Coupons.Select(a => a.CouponNumber).ToList();
+
+            var date = DateTime.Now;
+            string year = date.Year.ToString().Substring(2, 2);
+            string month = date.Month.ToString().PadLeft(2, '0');
+
+            int count = 1;
+            string CouponNum = GetCouponNums(year, month, count);
+
+            while (CouponsNums.Contains(CouponNum))
+            {
+                count++;
+                CouponNum = GetCouponNums(year, month, count);
+            }
+
+            ViewBag.CouponNum = CouponNum;
+
+            var Sellers = db.Sellers.Where(x=>x.SellerId== SellerId).Select(x=>x.StoreName).FirstOrDefault().ToString();
+
+            ViewBag.StoreName = Sellers;
+
             return View();
         }
-
-        public ActionResult MyPointList()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CouponCreate([Bind(Include = "CouponId,SellerId,CouponNumber,CouponName,CouponDetails,Quantity,ReceivedQuantity,CouponTerms,CouponType,Discount,Storewide,StartTime,ClaimDeadline,EndTime,EventStatus")] CouponVM vm)
         {
-            return View();
+            int sellerId = 1;
+                sellerId = db.Sellers.Where(s => s.StoreName == vm.Storewide).Select(s => s.SellerId).FirstOrDefault();
+
+            var coupon = new Coupon
+            {
+                CouponId = vm.CouponId,
+                SellerId = sellerId,
+                CouponNumber = vm.CouponNumber,
+                CouponName = vm.CouponName,
+                CouponDetails = vm.CouponDetails,
+                Quantity = vm.Quantity,
+                ReceivedQuantity = 0,
+                CouponTerms = vm.CouponTerms,
+                CouponType = vm.CouponType,
+                Discount = vm.Discount,
+                Storewide = vm.Storewide,
+                StartTime = vm.StartTime,
+                ClaimDeadline = vm.ClaimDeadline,
+                EndTime = vm.EndTime,
+                EventStatus = vm.EventStatus
+            };
+
+            if (ModelState.IsValid)
+            {
+                db.Coupons.Add(coupon);
+                db.SaveChanges();
+                return RedirectToAction("CouponList");
+            }
+
+            return View(coupon);
+        }
+
+        public ActionResult MyPointList(int? SellerId)
+        {
+            int? sellerId = Session["SellerId"] as int?;
+            if(sellerId == null) 
+            { return RedirectToAction("Login", "SellerLogin");}
+            else
+            { sellerId =(int) Session["SellerId"]; }
+
+            SellerId = sellerId;
+
+            var point = new ADPoint();
+            var model = ADPointChange.BuyPoint(point, (int)SellerId);
+            ViewBag.List = db.ADPoints.Where(s => s.SellerId == SellerId).OrderByDescending(d => d.PurchaseTime).ToList();
+            ViewBag.SellerPoint = db.Sellers.Where(s => s.SellerId == SellerId).Select(p => p.ADPoints).FirstOrDefault();
+            return View(model);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MyPointList([Bind(Include = "ADPointId,SellerId,GUINumber,PurchaseTime,ADPoints,PaymentStatus")] ADPointVM vm)
+        {
+
+            var sellerToUpdate = db.Sellers.FirstOrDefault(x => x.SellerId == vm.SellerId);
+            if (sellerToUpdate != null)
+            {
+                sellerToUpdate.ADPoints = sellerToUpdate.ADPoints + vm.ADPoints;
+                db.Entry(sellerToUpdate).State = EntityState.Modified;
+                db.ADPoints.Add(ADPointChange.VM2ADPoint(vm));
+                db.SaveChanges();
+                return RedirectToAction("MyPointList");
+            }
+
+            return View(vm);
         }
 
         public ActionResult MyPointBuy()
@@ -160,5 +264,11 @@ namespace prjEShopping.Controllers
             return View();
         }
 
+        //-------------其他方法----------------
+        //生優惠券號
+        public string GetCouponNums(string year, string month, int count)
+        {
+            return $"C{year}{month}{count.ToString().PadLeft(2, '0')}";
+        }
     }
 }
