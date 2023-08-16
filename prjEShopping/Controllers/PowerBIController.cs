3b234a6a0aab14c2ecb079b9b20c3a869c9f169a
SieJH <sjh.jason@gmail.com>
2023-08-16 20:26:13 +0800

    POWERBI 的View生成

---
+++ prjEShopping/Controllers/PowerBIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjEShopping.Controllers
+{
+    public class PowerBIController : Controller
+    {
+        // GET: PowerBI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
